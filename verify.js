a6701d26603e71678dfa16373ccdd61953a277f1
Jan Pochyla <jpochyla@gmail.com>
2014-07-02 14:00:27 +0200

    Fix vars

--- verify.js
+++ verify.js
@@ -1,8 +1,8 @@
-//keyserverURL = "http://subset.pool.sks-keyservers.net"
-keyserverURL = 'https://sks.disunitedstates.com'
+// var keyserverURL = "http://subset.pool.sks-keyservers.net";
+var keyserverURL = 'https://sks.disunitedstates.com';
 
 function parse_url() {
-    signature = window.location.href.split(/#(.+)?/)[1];
+    var signature = window.location.href.split(/#(.+)?/)[1];
     if (typeof signature === 'undefined') {
         return false;
     }
@@ -11,22 +11,22 @@ function parse_url() {
 }
 
 function verify_signature(signature) {
-    sig = openpgp.cleartext.readArmored(signature);
-    identity = "0x" + sig.getSigningKeyIds()[0].toHex();
+    var sig = openpgp.cleartext.readArmored(signature);
+    var identity = "0x" + sig.getSigningKeyIds()[0].toHex();
 
     var key = openhkp.op_get(keyserverURL, identity, 0);
-    keys = openpgp.key.readArmored(key);
-    key = keys.keys[0];
+    var keys = openpgp.key.readArmored(key);
+    var key = keys.keys[0];
 
     if (key.verifyPrimaryKey() != openpgp.enums.keyStatus.valid) {
         alert("Invalid key!");
-        return;
+        return false;
     }
 
-        var verified = sig.verify(keys.keys);
+    var verified = sig.verify(keys.keys);
     if (!verified || verified.length <= 0 || !verified[0].valid) {
         alert("Verifying signature: FAILED!");
-        return;
+        return false;
     }
 
     var identities = '';
@@ -34,7 +34,7 @@ function verify_signature(signature) {
         identities += user.userId.userid + "\n";
     });
 
-    fingerprint = key.primaryKey.fingerprint;
+    var fingerprint = key.primaryKey.fingerprint;
     alert("VALID signature from\n\n" + identities + "\n(0x" + fingerprint + ")");
     return true;
 }
