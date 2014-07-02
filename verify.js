3f92c584bfc86f1cbe5562f4d2af6744aa77d8e7
slush0 <slush@satoshilabs.com>
2014-07-02 03:49:51 +0200

    Pretties output of signer identity, added check for valid key.

--- verify.js
+++ verify.js
@@ -6,17 +6,25 @@ function verify_signature(identity, signature) {
     var key = openhkp.op_get(keyserverURL, identity, 0);
     keys = openpgp.key.readArmored(key);
     key = keys.keys[0];
-    user = key.getPrimaryUser();
-    key.verifyPrimaryKey() == openpgp.enums.keyStatus.valid;
+
+    if (key.verifyPrimaryKey() != openpgp.enums.keyStatus.valid) {
+        alert("Invalid key!");
+        return;
+    }
 
     sig = openpgp.cleartext.readArmored(signature);
     var verified = sig.verify(keys.keys);
     if (!verified || verified.length <= 0 || !verified[0].valid) {
-      alert("Verifying signature: FAILED!");
-      return;
+        alert("Verifying signature: FAILED!");
+        return;
     }
-    alert("Verifying signature: OK!");
 
+    var identities = '';
+    key.users.forEach(function(user) {
+        identities += user.userId.userid + "\n";
+    });
+
+    alert("VALID signature from\n\n" + identities);
     return true;
 }
 
